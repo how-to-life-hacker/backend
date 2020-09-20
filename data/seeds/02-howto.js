df960fa43f38998a0f131ae3623816a4d20095ca
Galo Sandoval <galosan92@gmail.com>
2020-09-20 15:22:36 -0700

    starting fresh

--- data/seeds/02-howto.js
+++
@@ -1,8 +0,0 @@
-
-exports.seed = function(knex, Promise) {
-  return knex('howto').insert([
-    { name: "Build a website", user_id: 3 }, 
-    { name: "Get better sleep", user_id: 3 }, 
-    { name: "Meditate", user_id: 4 }
-  ]);
-};
