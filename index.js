82f48734edcfc704cb0468e677559786aefdd199
Julian Leon <jleon1992@gmail.com>
2020-09-21 14:36:50 -0400

    attemptingtodeploy

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const server = require('./api/server')
 
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 4000;
 
 server.listen(port, () => {
-    console.log('server running on port 5000')
+    console.log('server running on port 4000')
 })
